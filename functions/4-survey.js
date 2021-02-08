db16a9f0030c5fe598300325e36592e3534cdad2
girish <girishgodage@gmail.com>
2021-02-08 17:23:56 +0530

    Added survey Functions

---
+++ functions/4-survey.js
@@ -0,0 +1,76 @@
+//domain/.netlify/functions/4-survey
+//domain/api/4-survey
+
+require("dotenv").config();
+const Airtable = require("airtable-node");
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base("app1lUqcqwYqDKdY7")
+  .table("survey");
+
+exports.handler = async (event, context, cb) => {
+  const method = event.httpMethod;
+  if (method === "GET") {
+    try {
+      const { records } = await airtable.list();
+      const survey = records.map((item) => {
+        const { id } = item;
+        const { room, votes } = item.fields;
+        return { id, room, votes };
+      });
+      return {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        statusCode: 200,
+        body: JSON.stringify(survey),
+      };
+    } catch (error) {
+      return {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        statusCode: 500,
+        body: "Server Error",
+      };
+    }
+  }
+  if (method === "PUT") {
+    try {
+      const { id, votes } = JSON.parse(event.body);
+      if (!id || !votes) {
+        return {
+          statusCode: 400,
+          body: "Please provide id and votes values",
+        };
+      }
+      const fields = { votes: Number(votes) + 1 };
+      const item = await airtable.update(id, { fields });
+      console.log(item);
+      if (item.error) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify(item),
+        };
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify(item),
+      };
+    } catch (error) {
+      return {
+        statusCode: 400,
+        body: "Please provide id and votes values",
+      };
+    }
+  }
+
+  // Default Response
+  return {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    statusCode: 405,
+    body: "Only GET and PUT Requests Allowed",
+  };
+};
